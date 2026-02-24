009535379d6adc3bddd9f8ab1044708d178a9454
SpoopyEd <eduardoaguilera1031@gmail.com>
2026-02-24 01:54:38 -0800

    Fraction class created, main user input created

---
+++ Fraction.java
@@ -0,0 +1,18 @@
+// Eduardo Aguilera
+// Cs145
+// 2/24/26
+// Assignment 7 calculate least common denominator to add 2 fractions using recursion
+// Fraction class, creates fraction object with numerator and denominator
+public class Fraction {
+    int n;
+    int d;
+    public Fraction() { // constructs empty fraction
+    }
+    public Fraction(int n, int d) { // constructs fraction using paramaters
+        this.n = n;
+        this.n = d;
+    }
+    public void changeD() {
+        // what happens to the numerator when the denominator changes?
+    }
+}
