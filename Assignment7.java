51a4a257c7eb7af42ee917856aeb402aaa8d6c84
SpoopyEd <eduardoaguilera1031@gmail.com>
2026-02-24 18:55:41 -0800

    Least common denominator method created through recursion

--- Assignment7.java
+++ Assignment7.java
@@ -7,9 +7,33 @@ import java.util.Scanner;
 public class Assignment7 {
     public static Scanner input = new Scanner(System.in);  // Create a Scanner object
     public static void main(String[] args) {
-        prompt();
+        System.out.println(leastCommon(3,28));
     }
-    public static void prompt() {
+    public static int leastCommon(int d1, int d2) {
+        int lcd = 0;
+        int largest = 0;
+        int smallest = 0;
+        if (d1 == d2) {
+            lcd = d1;
+            return lcd;
+        } else {
+            if (d1 > d2) { // finds smallest and largest
+                largest = d1;
+                smallest = d2;
+            } else {
+                largest = d2;
+                smallest = d1;
+            }
+
+            if (largest % smallest == 0) {
+                lcd = largest;
+                return lcd;
+            } else { // if largest not divisble by smallest then multiply them = lcd
+                return leastCommon(d1 * d2, d2); 
+            }
+        }
+    }
+    public static void prompt() { // will need to change later
         System.out.println("What's the numerator for the 1st fraction you'd like to add?");
         int n1 = input.nextInt();
         // catch non int
